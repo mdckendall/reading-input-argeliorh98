0b31ededaf8252afe5d014df2e479f8555ea6126
argeliorh98 <122834690+argeliorh98@users.noreply.github.com>
2023-06-21 23:06:42 +0000

    work done

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
-  }
+    Scanner in = new Scanner (System.in);
+    String userWord;
+    userWord  = in.nextLine();
+    System.out.println (userWord.length());
+}
 }
